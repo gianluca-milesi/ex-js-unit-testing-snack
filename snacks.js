f1096f81f9f0c5f6ea66646ed34e586a6a52f651
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-04-15 18:22:43 +0200

    ADD: Snack 3. Test average

--- snacks.js
+++ snacks.js
@@ -7,4 +7,10 @@ function createSlug(str) {
     return str.toLowerCase()
 }
 
-module.exports = { getInitials, createSlug }
+function average(numbers) {
+    let sum = 0
+    numbers.forEach(num => sum += num)
+    return sum / numbers.length
+}
+
+module.exports = { getInitials, createSlug, average }
--- snacks.test.js
+++ snacks.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug } = require("./snacks.js")
+const { getInitials, createSlug, average } = require("./snacks.js")
 
 //Snack 1
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
@@ -10,4 +10,9 @@ test("La funzione getInitials restituisce le iniziali di un nome completo.", () 
 //Snack 2
 test("La funzione createSlug restituisce una stringa in lowercase.", () => {
     expect(createSlug("Ciao")).toBe("ciao")
+})
+
+//Snack 3
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+    expect(average([10, 20, 30])).toBe(20)
 })
